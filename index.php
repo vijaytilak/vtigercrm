b5596e1a4a6f31c87947fb00bc6076bfad38bec7
Vijay Tilak <vijaykrishnatilak@gmail.com>
2024-11-15 03:54:35 +1300

    Enabled Error Reporting

--- index.php
+++ index.php
@@ -8,6 +8,12 @@
  * All Rights Reserved.
  ************************************************************************************/
 
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__FILE__) . '/vtiger_errors.log');
+
 if (!file_exists("vendor/autoload.php")) {
     echo "Please install composer dependencies.";
     exit;
